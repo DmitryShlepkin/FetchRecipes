1508819ba1b1168de138f65d7d7d0cd984f2b397
Dmitry Shlepkin <acidl33t@gmail.com>
2025-02-03 11:37:34 -0500

    Initial Commit

---
+++ FetchRecipesTests/FetchRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesTests.swift
+//  FetchRecipesTests
+//
+//  Created by Dmitry Shlepkin on 2/3/25.
+//
+
+import Testing
+@testable import FetchRecipes
+
+struct FetchRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
